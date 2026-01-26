cbd875c832ab44999da92d3d5afc83ec72e77b7c
ChuRit <churit@naver.com>
2026-01-26 16:42:35 +0900

    빌드 오류 수정

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,9 @@ module.exports = async function(env, argv) {
     'expo-router': false,
   };
   
+  // _expo 폴더 없이 바로 빌드되도록 설정
+  config.output.publicPath = '';
+  config.output.path = config.output.path.replace('/_expo', '');
+  
   return config;
 };
